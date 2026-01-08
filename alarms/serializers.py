705944e1964b97b54e828d866ac0910fe29c1684
Aleksander <251544@edu.p.lodz.pl>
2026-01-08 22:20:03 +0100

    serializery dla views

---
+++ alarms/serializers.py
@@ -0,0 +1,75 @@
+from rest_framework import serializers
+from .models import Alert, AlertRule, AlertComment, SZEBiUser
+
+
+class AlertRuleSerializer(serializers.ModelSerializer):
+    """Serializer dla reguł alarmów"""
+
+    class Meta:
+        model = AlertRule
+        fields = [
+            'id',
+            'name',
+            'target_metric',
+            'operator',
+            'threshold_min',
+            'threshold_max',
+            'duration_seconds',
+            'priority'
+        ]
+        read_only_fields = ['id']
+
+
+class SZEBiUserSerializer(serializers.ModelSerializer):
+    """Serializer dla użytkowników SZEBi"""
+
+    class Meta:
+        model = SZEBiUser
+        fields = ['id', 'username', 'email']
+
+
+class AlertCommentSerializer(serializers.ModelSerializer):
+    """Serializer dla komentarzy alarmów"""
+
+    class Meta:
+        model = AlertComment
+        fields = ['text', 'timestamp']
+
+
+class AlertSerializer(serializers.ModelSerializer):
+    """Serializer dla alarmów"""
+    alert_rule = AlertRuleSerializer(read_only=True)
+    alert_comment = AlertCommentSerializer(read_only=True)
+    acknowledged_by = SZEBiUserSerializer(read_only=True)
+    closed_by = SZEBiUserSerializer(read_only=True)
+
+    status_display = serializers.CharField(
+        source='get_status_display', read_only=True)
+    priority_display = serializers.CharField(
+        source='get_priority_display', read_only=True)
+
+    class Meta:
+        model = Alert
+        fields = [
+            'id',
+            'alert_rule',
+            'alert_comment',
+            'triggering_value',
+            'timestamp_generated',
+            'timestamp_acknowledged',
+            'timestamp_closed',
+            'status',
+            'status_display',
+            'priority',
+            'priority_display',
+            'acknowledged_by',
+            'closed_by'
+        ]
+        read_only_fields = [
+            'id',
+            'timestamp_generated',
+            'timestamp_acknowledged',
+            'timestamp_closed',
+            'acknowledged_by',
+            'closed_by'
+        ]
