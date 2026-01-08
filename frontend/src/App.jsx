be193cf68ee3dd72cbe55b029a33f175a6bcb5c1
Aleksander <251544@edu.p.lodz.pl>
2026-01-08 22:21:33 +0100

    odwolanie do strony alarmow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
+import AlarmsPage from './pages/AlarmsPage';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="optimization" element={<div className="p-8 text-center text-gray-400">Optimization Module Placeholder</div>} />
-          <Route path="alarms" element={<div className="p-8 text-center text-gray-400">Alarms Module Placeholder</div>} />
+          <Route path="alarms" element={<AlarmsPage />} />
           <Route path="analysis" element={<div className="p-8 text-center text-gray-400">Analysis Module Placeholder</div>} />
           <Route path="*" element={<div className="p-8 text-center text-gray-400">404 - Not Found</div>} />
         </Route>
